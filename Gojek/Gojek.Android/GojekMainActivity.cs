fa4e2e04baa8b9fce21dd668b849a594b0c841ee
kenny_dizi <chitrung09t2@gmail.com>
2021-05-12 13:04:03 +0700

    initial commit

---
+++ Gojek/Gojek.Android/GojekMainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.Runtime;
+using Android.OS;
+
+namespace Gojek.Droid
+{
+    [Activity(Label = "Gojek", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true,
+        ConfigurationChanges = Android.Content.PM.ConfigChanges.ScreenSize |
+                               Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.UiMode |
+                               Android.Content.PM.ConfigChanges.ScreenLayout |
+                               Android.Content.PM.ConfigChanges.SmallestScreenSize)]
+    public class GojekMainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        public GojekMainActivity()
+        {
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new GojekApp());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions,
+            [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ Gojek/Gojek/GojekApp.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms;
+
+namespace Gojek
+{
+    public partial class GojekApp : Application
+    {
+        public GojekApp()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
