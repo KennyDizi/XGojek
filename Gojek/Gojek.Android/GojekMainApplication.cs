2c65a03f2a6e942c50ccc87547ebb17583530461
kenny_dizi <chitrung09t2@gmail.com>
2021-05-14 20:32:13 +0700

    add GojekMainApplication class

---
+++ Gojek/Gojek.Android/GojekMainApplication.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Runtime;
+using System;
+using Plugin.CurrentActivity;
+
+namespace Gojek.Droid
+{
+#if DEBUG
+[Application(Debuggable = true)]
+#else
+    [Application(Debuggable = false)]
+#endif
+    public class GojekMainApplication : Application
+    {
+        public GojekMainApplication(IntPtr handle, JniHandleOwnership transer)
+            : base(handle, transer)
+        {
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+            CrossCurrentActivity.Current.Init(this);
+        }
+    }
+}
